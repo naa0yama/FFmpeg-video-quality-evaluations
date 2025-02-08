027b2a3055fda045a7b15a7a5c27f26afdee16eb
Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>
2025-02-08 21:42:49 +0900

    Add: assets/h264_qsv_bf_ref_gq

--- src/ffmpegvqe/qp_gen.sh
+++ src/ffmpegvqe/qp_gen.sh
@@ -2,15 +2,16 @@
 
 # パラメータの範囲を定義
 CMD="${1}"
-START=23
-END=33
+START=25
+STEP=3
+END=38
 
 # min_qp_iのループ
-for min_qp_i in $(seq ${START} ${END}); do
+for min_qp_i in $(seq ${START} 1 ${END}); do
   # min_qp_pのループ
-  for min_qp_p in $(seq ${START} ${END}); do
+  for min_qp_p in $(seq ${START} ${STEP} ${END}); do
     # min_qp_bのループ
-    for min_qp_b in $(seq ${START} ${END}); do
+    for min_qp_b in $(seq ${START} ${STEP} ${END}); do
       # パターンを出力
       echo "${CMD} -min_qp_i ${min_qp_i} -min_qp_p ${min_qp_p} -min_qp_b ${min_qp_b}"
     done
