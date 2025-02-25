084ea7d009bc801acb2ef23e31d7280eea7f1c36
Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>
2025-02-26 00:09:30 +0900

    add assets libx265, libsvtav1
    Signed-off-by: Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>

--- src/ffmpegvqe/entrypoint.py
+++ src/ffmpegvqe/entrypoint.py
@@ -844,24 +844,28 @@ def getcsv(datafile: dict) -> None:
 def compress_files(dst: Path, files: list) -> None:
     """Create Tar Archive."""
     if not files:
-        print("Compress file not found.")  # noqa: T201
+        print("[ARCHIVE] Compress file not found.")  # noqa: T201
         return
-    archive_name = f"{dst}/vmaf_archive.xz"
+    archive_name = f"{dst.parent}/vmaf_archive.tar.xz"
 
-    print(f"\n\nCreate archive file: {archive_name}")  # noqa: T201
+    print(f"\n\n[ARCHIVE] Create archive file: {archive_name}")  # noqa: T201
     with tarfile.open(archive_name, "w:xz") as tar:
         __length: int = len(files)
         for __index, file_path in enumerate(files):
             tar.add(file_path, arcname=file_path.name)
             print(  # noqa: T201
-                f"{__index + 1:0>4}/{__length:0>4} ({(__index + 1) / __length:>7.2%})"
+                f"[ARCHIVE] {__index + 1:0>4}/{__length:0>4} ({(__index + 1) / __length:>7.2%})"
                 f" Add compress file: {file_path.name}",
             )
-    print("Create archive file done.")  # noqa: T201
+    print("[ARCHIVE] Create archive file done.")  # noqa: T201
 
     for file in files:
         Path(file).unlink()
-        print(f"remove : {file}")  # noqa: T201
+        print(f"[ARCHIVE] remove : {file}")  # noqa: T201
+
+    if dst.exists():
+        dst.rmdir()
+        print(f"[ARCHIVE] rmdir  : {dst}")  # noqa: T201
 
 
 def archive() -> None:
@@ -870,7 +874,9 @@ def archive() -> None:
     __configfile: Path = Path(args.config)
     __basedir: Path = Path(f"./videos/dist/{__configfile.name.replace('.yml', '')}")
     __datafile: Path = Path(f"{__configs['configs']['datafile']}")
-    __datafilecsv: Path = Path(f"{__datafile}".replace(".json", ".csv"))
+    __datafilecsv_all: Path = Path(f"{__datafile}".replace(".json", "_all.csv"))
+    __datafilecsv_gby_option: Path = Path(f"{__datafile}".replace(".json", "_gby_option.csv"))
+    __datafilecsv_gby_type: Path = Path(f"{__datafile}".replace(".json", "_gby_type.csv"))
     __assetdir: Path = Path(f"./assets/{__basedir.name}/logs")
     __assetdir.mkdir(parents=True, exist_ok=True)
 
@@ -883,38 +889,44 @@ def archive() -> None:
 
     if (__hashs) != len(__data):
         print(  # noqa: T201
-            f"\n\ndatafile {__datafile} is hashs {__hashs} to encodes {len(__data)}.\n"
+            f"[ARCHIVE] \n\ndatafile {__datafile} is hashs {__hashs} to encodes {len(__data)}.\n"
             "archive process stop.",
         )
         return
 
     """move to __assetdir"""
-    __archive_files: list = [
-        __file for __file in __basedir.glob("*.(json|log)") if __file.is_file()
-    ]
-    for file_path in __archive_files:
-        if file_path.is_file():
-            shutil.move(f"{file_path}", f"{__assetdir}/{file_path.name}")
-            print(f"move: {file_path} to {__assetdir}/{file_path.name}")  # noqa: T201
+    __archive_files: list = []
+    for _ext in [".json", ".log"]:
+        for file_path in __basedir.glob(f"*{_ext}"):
+            if file_path.is_file():
+                shutil.move(f"{file_path}", f"{__assetdir}/{file_path.name}")
+                __archive_files.append(Path(f"{__assetdir}/{file_path.name}"))
+                print(f"[ARCHIVE] move: {file_path} to {__assetdir}/{file_path.name}")  # noqa: T201
 
-    """archive files"""
+    """archive vmaf files"""
     compress_files(dst=__assetdir, files=__archive_files)
 
     """move to configfile, csv datafile"""
-    for file in [__configfile, __datafile, __datafilecsv]:
+    for file in [
+        __configfile,
+        __datafile,
+        __datafilecsv_all,
+        __datafilecsv_gby_option,
+        __datafilecsv_gby_type,
+    ]:
         shutil.move(f"{file}", f"{__assetdir.parent}/{file.name}")
-        print(f"move: {file} to {__assetdir.parent}/{file.name}")  # noqa: T201
+        print(f"[ARCHIVE] move: {file} to {__assetdir.parent}/{file.name}")  # noqa: T201
 
     with Path(f"{__assetdir.parent}/{__configfile.name}").open("r") as __file:
         ____data = yaml.load(__file)
     ____data["configs"]["datafile"] = f"{__assetdir.parent}/{__datafile.name}"
 
-    print(f"dataset name overwrite file: {__assetdir.parent}/{__datafile.name}")  # noqa: T201
+    print(f"[ARCHIVE] dataset name overwrite file: {__assetdir.parent}/{__datafile.name}")  # noqa: T201
     with Path(f"{__assetdir.parent}/{__configfile.name}").open("w") as ___file:
         yaml.dump(____data, ___file)
 
     if __basedir.exists():
-        print(f"remove dist directory: {__basedir}")  # noqa: T201
+        print(f"[ARCHIVE] remove dist directory: {__basedir}")  # noqa: T201
         __basedir.rmdir()
 
 
